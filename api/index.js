ca64d4879435c6962c26eed00e6803c479ae697b
Redf0xD <ricardoprato36@gmail.com>
2022-04-05 10:16:30 -0400

    change config

--- api/index.js
+++ api/index.js
@@ -19,6 +19,6 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const {conn} = require("./src/db.js");
-
+const PORT = process.env.PORT;
 // Syncing all the models at once.
 conn.sync({force: true}).then(() => server.listen(PORT));
