702ee528ef449f73c5d3148263994ace9e8d4f44
Redf0xD <ricardoprato36@gmail.com>
2022-04-05 09:49:16 -0400

    change config

--- api/src/db.js
+++ api/src/db.js
@@ -27,13 +27,10 @@ let sequelize =
         },
         ssl: true,
       })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-        {
-          logging: false,
-          native: false,
-        }
-      );
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+        logging: false,
+        native: false,
+      });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
