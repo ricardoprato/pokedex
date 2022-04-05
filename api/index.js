19327db19d0b4cb3ae3b143fe307dc0a04e79e83
Redf0xD <ricardoprato36@gmail.com>
2022-04-05 09:53:52 -0400

    change config

--- api/index.js
+++ api/index.js
@@ -21,4 +21,4 @@ const server = require("./src/app.js");
 const {conn} = require("./src/db.js");
 
 // Syncing all the models at once.
-conn.sync({force: true}).then(() => server.listen(process.env.PORT));
+conn.sync({force: true}).then(() => server.listen(PORT));
--- api/src/db.js
+++ api/src/db.js
@@ -3,6 +3,7 @@ const {Sequelize} = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+
 let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
@@ -27,10 +28,10 @@ let sequelize =
         },
         ssl: true,
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
-        logging: false,
-        native: false,
-      });
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+        {logging: false, native: false}
+      );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
