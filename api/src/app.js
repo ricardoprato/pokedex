76a480b55836a7d571625603962791f5f3c9267c
Redf0xD <ricardoprato36@gmail.com>
2022-04-05 15:09:10 -0400

    update config

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pi-pokemon-orpin.vercel.app/"
+    "https://pi-pokemon-henna.vercel.app/"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
