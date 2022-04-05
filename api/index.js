9222bf9486709cb1cd459d14e5ccf6932c3d34e5
Redf0xD <ricardoprato36@gmail.com>
2022-04-05 10:41:23 -0400

    change package.json

--- api/index.js
+++ api/index.js
@@ -19,6 +19,8 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const {conn} = require("./src/db.js");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
-conn.sync({force: true}).then(() => server.listen(PORT));
+conn
+  .sync({force: true})
+  .then(() => server.listen(PORT => console.log(`Listening on port ${PORT}`)));
